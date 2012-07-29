55aecc85bba9aab06d12f7126e1dd31fcdb8cd3d
Zero <john.royer@gmail.com>
2012-07-29 12:58:33 +0800

    Adjust List Table

--- application/views/user/list.php
+++ application/views/user/list.php
@@ -1,32 +1,56 @@
 <?php
-   $this->load->view('header');  
-   $this->load->view('navbar'); 
-   $this->load->helper('url');  
+$this->load->view('header');  
+$this->load->view('navbar'); 
+$this->load->helper('url');  
 ?>
 
    <h2>Hello, <?php echo $user['id']; ?></h2>
 
    <p style="margin-top: 25px;"></p>
-   
+
    <table class="table">
       <tbody>
          <tr>
-            <td>Animation Name</td>
-            <td>Vol</td>
-            <td>Actions</td>
-         <tr>
+            <td>動畫</td>
+            <td>字幕組</td>
+            <td>集數</td>
+            <td></td>
+         </tr>
 
-      <tr>
-         <td>CLANNAD</td>
-         <td style="width: 200px;"> 
-            <span style="margin-right: 20px;">4</span>
-            <a href="#"><i class="icon-plus"></i></a>
-            <a href="#"><i class="icon-minus"></i></a>
-         </td>
-         <td style=""> 
-            <a class="action-link" href="#"><i class="action-icon icon-ok"></i>Finished !!!</a>
-         </td>
-      </tr>
+         <tr>
+            <td>宇宙超級無敵媒人能比的非常多字的動畫名稱</td>
+            <td>liuming</td>
+            <td class="volume">8</td>
+            </td>
+            <td style=""> 
+               <a href="#"><i class="action-icon icon-plus"></i></a>
+               <a href="#"><i class="action-icon icon-minus"></i></a>
+               <a class="action-link" href="#"><i class="action-icon icon-ok"></i>看完了！</a>
+            </td>
+         </tr>
+
+         <tr>
+            <td>CLANNAD</td>
+            <td>DHR</td>
+            <td class="volume">18</td>
+            <td style=""> 
+               <a href="#"><i class="action-icon icon-plus"></i></a>
+               <a href="#"><i class="action-icon icon-minus"></i></a>
+               <a class="action-link" href="#"><i class="action-icon icon-ok"></i>看完了！</a>
+            </td>
+         </tr>
+
+         <tr>
+            <td>海賊王</td>
+            <td>N/A</td>
+            <td class="volume">511</td>
+            </td>
+            <td style=""> 
+               <a href="#"><i class="action-icon icon-plus"></i></a>
+               <a href="#"><i class="action-icon icon-minus"></i></a>
+               <a class="action-link" href="#"><i class="action-icon icon-ok"></i>看完了！</a>
+            </td>
+         </tr>
       </tbody>
    </table>
 
