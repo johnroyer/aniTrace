abbde4f771b754a9859fa65ba5686d48942e6303
Zero <john.royer@gmail.com>
2012-08-06 10:43:04 +0800

    Page Refresh
    Refresh page every 1 hour if user logged in.

--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,12 @@
 <!doctype html>
 <html>
    <head>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8">
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+      <!-- Refresh every 1 hour -->
+      <?php if( $loggedin == true ): ?>
+         <meta http-equiv="refresh" content="3600" />
+      <?php endif; ?>
 
       <title>aniTrace</title>
       <link rel="stylesheet" href="<?php echo base_url(); ?>/css/bootstrap.min.css" type="text/css" charset="utf-8">
