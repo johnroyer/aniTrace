6ea3ad62c31d7771c1bcf796262eaa8171600202
Zero <john.royer@gmail.com>
2012-08-25 14:41:28 +0800

    jQuery update
    Update jQuery to version 1.8.0

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,7 +3,7 @@
 <script type="text/javascript" charset="utf-8">
    var site_url = '<?php echo site_url(); ?>';
 </script>
-<script src="<?php echo base_url(); ?>/js/jquery-1.7.2.min.js"></script>
+<script src="<?php echo base_url(); ?>/js/jquery-1.8.0.min.js"></script>
 <script src="<?php echo base_url(); ?>/js/jquery.validate.min.js"></script>
 <script src="<?php echo base_url(); ?>/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url(); ?>/js/script.js"></script>
