9b27247084e5480b7aa22803acd16d02b60a9d04
Zero <john.royer@gmail.com>
2012-09-06 17:39:25 +0800

    Add Script Getting Animation List
    Javascript that getting animation list via ajax on document ready.
    
    Function renewAniList() will clear all data and put fetched
    animation into table. But columns that have action icons will get error
    on data insertion. Icons are all overwritten.

--- js/ani.js
+++ js/ani.js
@@ -4,14 +4,32 @@
  *
  *****************************************************************************/
 
+$('document').ready( getAniList() );
+
 function getAniList ( ) {
    $.ajax( {
       url: site_url + '/ajax/',
       dataType: 'json',
       error: function(){ console.log('Get animation list failed') },
-      success: renewList()
-   }
+      success: function(response){
+            renewList(response);
+         }
+   } );
 }
 
-function renewList (response) {
+function renewList( response ){
+   clearTable();
+   if( response === undefined )
+      console.log('response is empty');
+      for( aniId in response ){
+         $('#ani-list > tbody > tr:last').after( $('#row-template').clone().removeAttr('id') );
+         var $currRow = $('tr:last');
+         for( elem in response[aniId] ){
+            $currRow.find('> td.col-' + elem ).text( response[aniId][elem] );
+         }
+      }
+}
+
+function clearTable(){
+   $('#ani-list > tbody > tr:not(#row-template)').remove();
 }
