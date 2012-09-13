2933b46ebbe2b1ef7c8d277d24ce0fdcf6e3f646
Zero <john.royer@gmail.com>
2012-09-13 16:19:35 +0800

    Fix Javascript Path Error
    There is one more slash between base URL and path.

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,14 +4,14 @@
    var site_url = '<?php echo site_url(); ?>';
 </script>
 
-<script src="<?php echo base_url(); ?>/js/jquery-1.8.0.min.js"></script>
-<script src="<?php echo base_url(); ?>/js/jquery.validate.min.js"></script>
-<script src="<?php echo base_url(); ?>/js/jquery.tmpl.min.js"></script>
-<script src="<?php echo base_url(); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>js/jquery-1.8.0.min.js"></script>
+<script src="<?php echo base_url(); ?>js/jquery.validate.min.js"></script>
+<script src="<?php echo base_url(); ?>js/jquery.tmpl.min.js"></script>
+<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
 
-<script src="<?php echo base_url(); ?>/js/admin.js"></script>
-<script src="<?php echo base_url(); ?>/js/user.form.js"></script>
-<script src="<?php echo base_url(); ?>/js/ani.js"></script>
-<script src="<?php echo base_url(); ?>/js/script.js"></script>
+<script src="<?php echo base_url(); ?>js/admin.js"></script>
+<script src="<?php echo base_url(); ?>js/user.form.js"></script>
+<script src="<?php echo base_url(); ?>js/ani.js"></script>
+<script src="<?php echo base_url(); ?>js/script.js"></script>
 </body>
 </html>
