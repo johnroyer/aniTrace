3495ac1c7a9be561bd0db6a26273e0396d842502
Zero <john.royer@gmail.com>
2012-09-13 16:17:34 +0800

    Fix CSS Path Error
    There is one more slash between base URL and path.

--- application/views/header.php
+++ application/views/header.php
@@ -10,9 +10,9 @@
       <?php endif; ?>
 
       <title>aniTrace</title>
-      <link rel="stylesheet" href="<?php echo base_url(); ?>/css/bootstrap.min.css" type="text/css" charset="utf-8">
-      <link rel="stylesheet" href="<?php echo base_url(); ?>/css/font-awesome.css" type="text/css" charset="utf-8">
-      <link rel="stylesheet" href="<?php echo base_url(); ?>/css/style.css" type="text/css" charset="utf-8">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css" type="text/css" charset="utf-8">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>css/font-awesome.css" type="text/css" charset="utf-8">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" type="text/css" charset="utf-8">
    </head>
    <body>
 
