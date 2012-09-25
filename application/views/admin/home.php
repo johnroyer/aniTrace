db3e4cb5b0cd87acc1d99b329ebc91df39de083d
Zero <john.royer@gmail.com>
2012-09-26 02:36:50 +0800

    Change Text to Chinese
    Change texts in views to Chinese.

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -2,14 +2,14 @@
 <?php  $this->load->view('navbar');  ?>
 <?php  $this->load->helper('url');  ?>
 
-<h2>User Management</h2>
+<h2>使用者管理</h2>
 <p style="margin-top: 25px;"> </p>
 
 <div class="tabbable">
 
    <ul class="nav nav-tabs ">
-      <li class="<?php echo $tab_general; ?>"><a href="<?php echo site_url('admin/'); ?>">General</a></li>
-      <li class="<?php echo $tab_admin; ?>"><a href="<?php echo site_url('admin/superuser'); ?>">Admin</a></li>
+      <li class="<?php echo $tab_general; ?>"><a href="<?php echo site_url('admin/'); ?>">一般</a></li>
+      <li class="<?php echo $tab_admin; ?>"><a href="<?php echo site_url('admin/superuser'); ?>">管理員</a></li>
       <li>
          <form class="form-search admin-tabs-search" action="#" method="post" accept-charset="utf-8">
          <i class="icon-search admin-tabs-search-icon"></i>
@@ -24,9 +24,9 @@
          <thead>
             <tr>
                <td>ID</td>
-               <td>Username</td>
+               <td>帳號</td>
                <td>Email</td>
-               <td>Action</td>
+               <td>操作</td>
             </tr>
          </thead>
 
@@ -36,8 +36,8 @@
                <td class="row-username"></td>
                <td class="row-email"></td>
                <td class="row-action">
-                  <a href="<?php echo site_url('admin/deleteUser/'); ?>" class="action-link deleteUser"><i class="icon-trash action-icon"></i>Delete</a>
-                  <a href="<?php echo site_url('admin/edit/'); ?>" class="action-link editUser"><i class="icon-pencil action-icon"></i>Edit</a>
+                  <a href="<?php echo site_url('admin/deleteUser/'); ?>" class="action-link deleteUser"><i class="icon-trash action-icon"></i>刪除</a>
+                  <a href="<?php echo site_url('admin/edit/'); ?>" class="action-link editUser"><i class="icon-pencil action-icon"></i>編輯</a>
                </td>
             </tr>
 
@@ -47,8 +47,8 @@
                   <td><?php echo $u->username; ?></td>
                   <td><?php echo $u->email; ?></td>
                   <td>
-                     <a href="<?php echo site_url('admin/deleteUser/'.$u->id); ?>" class="action-link"><i class="icon-trash action-icon"></i>Delete</a>
-                     <a href="<?php echo site_url('admin/edit/'.$u->id); ?>" class="action-link"><i class="icon-pencil action-icon"></i>Edit</a>
+                     <a href="<?php echo site_url('admin/deleteUser/'.$u->id); ?>" class="action-link"><i class="icon-trash action-icon"></i>刪除</a>
+                     <a href="<?php echo site_url('admin/edit/'.$u->id); ?>" class="action-link"><i class="icon-pencil action-icon"></i>編輯</a>
                   </td>
                </tr>
             <?php endforeach; ?>
