ec2da2b6edf2398c8638226243f3c0a907655fd1
Zero <john.royer@gmail.com>
2012-11-02 13:10:20 +0800

    Add Contrtoller 'download'
    Watching anime, search and download do not usually happen in the same time.
    Split anime list and download list to different pages. Download page display
    download information only. Anime list display watching progress.

---
+++ application/controllers/download.php
@@ -0,0 +1,51 @@
+<?php
+
+class Ani extends CI_Controller
+{
+   
+   private $user;
+
+   function __construct( )
+   {
+      parent::__construct();
+      $this->load->helper('url');
+      $this->load->spark('ion_auth/2.2.4');
+      if( $this->ion_auth->logged_in() == TRUE ){
+         $this->user = $this->_getUserInfo();
+      }else{
+         redirect('');
+      }
+      $this->load->model('animation');
+      $this->animation->setUid( $this->user['id'] );
+   }
+
+   public function index()
+   {
+      $data['user'] = $this->user;
+      $data['loggedin'] = true;
+      $data['list'] = $this->animation->getList();
+      $this->load->view('animation/download', $data);
+   }
+
+   private function _getUserInfo($id=NULL)
+   {
+      $info = $this->ion_auth->user($id)->row();
+      if( $info == NULL )
+         return NULL;
+      $groups = $this->ion_auth->get_users_groups()->result();
+      $isAdmin = false;
+      foreach ($groups as $group) {
+         $list[] = $group->id;
+         if( $group->id == 1 )
+            $isAdmin = true;
+      }
+      $user =  array(
+         'id' => $info->id ,
+         'username' => $info->username,
+         'email' => $info->email,
+         'groups' => $list,
+         'isAdmin' => $isAdmin
+      );
+      return $user;
+   }
+}
